7d1509186b18ad97fa40d41aa83aae76786b2465
Prince Singh Chouhan <princesinghchouhan7470@gmail.com>
2024-03-03 01:41:24 +0530

    achievements done

--- src/component/pages/Achievements.jsx
+++ src/component/pages/Achievements.jsx
@@ -1,7 +1,68 @@
 import React from 'react'
 
 export default function Achievements() {
-  return (
-    <div>Achievements</div>
-  )
+	return (
+		<div className='mx-48 mt-16'>
+			<div className="text-9xl  fixed z-[-100] font-bold top-[20%] left-1/2 translate-x-[-50%]">
+				<div className='inline-block'>
+					ACHIEVEMENTS of the <span className='text-red'>INSTITUTE</span>
+				</div>
+			</div>
+
+			<div className='bg-black flex mt-[65vh] py-16'>
+				<div className='p-8 w-[75%] grid gap-8'>
+					<h2 className='text-4xl'>
+						School of Computers Students <span className='text-red'>Win First Place</span> and <span className='text-red'>Entrepreneurship Award</span> at <span className='text-red'>Robotex Asia 19</span> in Shanghai
+					</h2>
+					<p className='text-xl'>Students of  School of computers, represented India in Asiatic Robotics Competition- Robotex Asia  19 (the biggest robotics festival on the planet) at Shanghai China and secured first position. They were also awarded best performance award in entrepreneurship category.</p>
+				</div>
+
+				<div className='flex justify-center items-center'>
+					<img src="https://www.ipsacademy.org/assets/uploads/colleges/blog_1661423994.jpg" alt="" className='rounded-lg hover:scale-[1.5] transition duration-1000' />
+				</div>
+			</div>
+
+			<div className='bg-black flex py-16'>
+				<div className='flex justify-center items-center'>
+					<img src="https://www.ipsacademy.org/assets/uploads/colleges/blog_1661423994.jpg" alt="" className='rounded-lg hover:scale-[1.5] transition duration-1000' />
+				</div>
+
+				<div className='p-8 w-[75%] grid gap-8'>
+					<h2 className='text-4xl'>
+						School of Computers Students <span className='text-red'>Triumph</span> in <span className='text-red'>ROBOTEX 2019 International Championship</span>
+					</h2>
+					<p className='text-xl'>Students of School of Computers won international championship ROBOTEX 2019 in entrepreneur category and got best innovative idea award in December 2019 at Tallinn Estonia Europe.</p>
+				</div>
+			</div>
+
+			<div className='bg-black flex py-16'>
+				<div className='p-8 w-[75%] grid gap-8'>
+					<h2 className='text-4xl'>
+						School of Computers Wins <span className='text-red'>AICTE's Chhatra Vishwakarma Award</span>, Receives <span className='text-red'>Invitation to National Convention</span> in New Delh
+					</h2>
+					<p className='text-xl'>School of Computers have been selected in AICTE central and western region and qualified for the prestigious "Chhatra Vishwakarma Award". For this they were invited for National convention that was held at New Delhi.</p>
+				</div>
+
+				<div className='flex justify-center items-center'>
+					<img src="https://www.ipsacademy.org/assets/uploads/colleges/blog_1661423994.jpg" alt="" className='rounded-lg hover:scale-[1.5] transition duration-1000' />
+				</div>
+			</div>
+
+			<div className='bg-black flex py-16'>
+				<div className='flex justify-center items-center'>
+					<img src="https://www.ipsacademy.org/assets/uploads/colleges/blog_1661423994.jpg" alt="" className='rounded-lg hover:scale-[1.5] transition duration-1000' />
+				</div>
+
+				<div className='p-8 w-[75%] grid gap-8'>
+					<h2 className='text-4xl'>
+					Nomination of School of Computers, IPS Academy as <span className='text-red'>Remote Center for Future FDPs</span> with <span className='text-red'>E & ICT Academy, NIT Patna</span>
+					</h2>
+					<p className='text-xl'>School of Computers, IPS Academy is nominated for Remote center for upcoming FDPs under the E & ICT Academy, NIT PATNA.</p>
+				</div>
+			</div>
+
+
+		</div>
+
+	)
 }
