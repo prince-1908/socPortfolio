89203f1a7f098f0dd6a50ffc79fc9962aed28b98
Prince Singh Chouhan <princesinghchouhan7470@gmail.com>
2024-03-03 13:15:20 +0530

    facilities

--- src/component/pages/Facilities.jsx
+++ src/component/pages/Facilities.jsx
@@ -1,17 +1,45 @@
 import React from 'react'
 
 export default function Facilities() {
-  return (
-    <div>
-      <div className="text-9xl  fixed z-[-100] font-bold top-[20%] left-1/2 translate-x-[-50%]">
-        <div className='inline-block'>
-          FACIL<span className='text-red'>ITIES</span>
-        </div>
-      </div>
+	return (
+		<div className='mx-48'>
+			<div className="text-9xl  fixed z-[-100] font-bold top-[20%] left-1/2 translate-x-[-50%]">
+				<div className='inline-block'>
+					FACIL<span className='text-red'>ITIES</span>
+				</div>
+			</div>
 
-      <div>
-        
-      </div>
-    </div>
-  )
+			<div className='mt-[25vh] text-5xl text-justify py-16 bg-black'>
+				Welcome to IPS Academy, the premier destination for Computer Application courses in Indore! At IPS, we pride ourselves on offering top-notch education alongside a range of facilities designed to enrich the learning experience for both students and faculty.
+			</div>
+
+			<div className='py-8 text-4xl bg-black'>
+				Here's what sets us apart:
+			</div>
+
+			<div className='py-8 text-4xl bg-black'>
+				State-of-the-art Facilities:
+			</div>
+
+			<div className='py-8 text-2xl bg-black mx-36 text-justify'>
+				Enjoy seamless connectivity with our free Wi-Fi facility available across the campus, ensuring you're always connected and up-to-date with your studies.
+			</div>
+
+			<div className='py-8 text-2xl bg-black mx-36 text-justify'>
+				Immerse yourself in the world of Artificial Intelligence in our cutting-edge AI lab. Equipped with the latest technology and resources, this lab provides students with hands-on experience in AI, preparing them for the demands of the industry.
+			</div>
+
+			<div className='py-8 text-2xl bg-black mx-36 text-justify'>
+				As an NPTEL Local Chapter, we provide access to a plethora of online courses for students and faculty alike. Whether you're looking to expand your knowledge or delve into a new subject area, our partnership with NPTEL opens doors to endless learning opportunities.
+			</div>
+
+			<div className='py-8 text-2xl bg-black mx-36 text-justify'>
+				Experience interactive learning in our classrooms, each equipped with AV projectors. Engage with course material like never before, with multimedia capabilities enhancing your understanding and retention.
+			</div>
+
+			<div className='py-8 text-2xl bg-black text-justify'>
+				At IPS Academy, we go beyond traditional education to provide a holistic learning environment that nurtures growth and success. Join us and embark on a journey towards academic excellence and personal development.
+			</div>
+		</div>
+	)
 }
